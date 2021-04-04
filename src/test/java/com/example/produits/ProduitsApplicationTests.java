c13c7828349773b3246b915be1b4abcaedcf3d53
UserSina <benna.anis2@gmail.com>
2021-04-04 14:17:14 +0100

    Initial commit

---
+++ src/test/java/com/example/produits/ProduitsApplicationTests.java
@@ -0,0 +1,52 @@
+package com.example.produits;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.produits.entities.Produit;
+import com.example.produits.repos.ProduitRepository;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+	@Autowired
+	private ProduitRepository produitRepository;
+	
+	@Test
+	public void testCreateProduit() {
+		Produit prod = new Produit("PC Asus",1800.500,new Date());
+		produitRepository.save(prod);
+	}
+	
+	@Test
+	public void testFindProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		System.out.println(p);
+	}
+	
+	@Test
+	public void testUpdateProduit()
+	{
+		Produit p = produitRepository.findById(1L).get();
+		p.setPrixProduit(1000.0);
+		produitRepository.save(p);
+	}
+	
+	@Test
+	public void testDeleteProduit() {
+		produitRepository.deleteById(1L);
+	}
+	
+	@Test
+	public void testFindAllProduits() {
+		List<Produit> prods = produitRepository.findAll();
+		
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+}
