8497fc26ae730e82d6581f71141a11738b9f3e15
UserSina <benna.anis2@gmail.com>
2021-04-09 16:51:49 +0100

    Finish tp8

--- src/main/java/com/example/produits/security/SecurityConfig.java
+++ src/main/java/com/example/produits/security/SecurityConfig.java
@@ -1,19 +1,49 @@
 package com.example.produits.security;
 
+
+//import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//@Autowired
+	//private DataSource dataSource;
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder passwordEncoder = passwordEncoder();
 		
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+		
+		/*
+		System.out.println("Passwors Encoded BCRYPT :******************** ");
+		System.out.println(passwordEncoder.encode("123"));
+
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username , password , enabled from user where username =?")
+			.authoritiesByUsernameQuery(
+					"SELECT u.username, r.role " +
+					"FROM user_role ur, user u , role r " +
+					"WHERE u.user_id = ur.user_id AND ur.role_id = r.role_id AND u.username = ?")
+			.passwordEncoder(passwordEncoder)
+			.rolePrefix("ROLE_");
+		*/
+		
+		/*
 		auth.inMemoryAuthentication().withUser("admin")
 			.password(passwordEncoder.encode("123"))
 			.roles("ADMIN");
@@ -25,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser("user1")
 			.password(passwordEncoder.encode("123"))
 			.roles("USER");
+		*/
 	}
 	
 	@Override
@@ -36,8 +67,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/supprimerProduit","/modifierProduit","/updateProduit")
 			.hasRole("ADMIN");
 		
+		//pour faire fonctionner Bootstrap
+		http.authorizeRequests().antMatchers("/webjars/**").permitAll();
+		http.authorizeRequests().antMatchers("/login").permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
-		http.formLogin();
+
+		http.formLogin().loginPage("/login");
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 	}
 	
